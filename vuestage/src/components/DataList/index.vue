127e0e51812cc99c7de2ee1ab479fea828ccb2f2
shenxingchao <864318466@qq.com>
2018-10-25 10:50:32 +0800

    update dashboard

---
+++ vuestage/src/components/DataList/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <ul>
+    <li v-if="title" class="ui-title">{{ title }}</li>
+    <li v-for="(item,index) in data" :key="index">
+      <a v-if="item.href" :href="item.href" target="_blank">
+        {{ item[row] }}
+      </a>
+      <template v-else>
+        {{ item[row] }}
+      </template>
+    </li>
+  </ul>
+</template>
+<script>
+export default{
+  props: {
+    data: {
+      type: Array,
+      default: null
+    },
+    row: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style scoped>
+  ul{
+    overflow: hidden;
+    list-style: none;
+    width: 100%;
+    padding: 0;
+    background: #ffffff;
+  }
+  ul li{
+    padding: 16px 14px;
+    border: 1px solid #ececec;
+    border-bottom: none;
+  }
+  ul li:last-child{
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    border-bottom: 1px solid #ececec;
+  }
+  ul li:first-child{
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+  }
+  .ui-title{
+    background: #409EFF;
+    color: #ffffff;
+    border: 1px solid #409EFF;
+  }
+
+</style>
--- vuestage/src/views/public/Dashboard.vue
+++ vuestage/src/views/public/Dashboard.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="app-container">有这几个字就够了</div>
+  <div class="app-container">
+    <el-row>
+      <el-col :xs="24" :sm="12" :md="6">
+        <DataList :data="List" title="知识点" row="name"/>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <script>
+import DataList from '@/components/DataList'
 export default {
   name: 'Dashboard',
+  components: { DataList },
   data() {
     return {
-
+      List: [
+        { name: '1.vue.js', href: 'https://cn.vuejs.org/' },
+        { name: '2.vue-admin-template', href: 'https://github.com/PanJiaChen/vue-admin-template' },
+        { name: '3.vue-router', href: 'https://router.vuejs.org/zh/' },
+        { name: '4.vuex', href: 'https://vuex.vuejs.org/zh/guide/' },
+        { name: '5.element-ui', href: 'http://element.eleme.io/#/zh-CN' },
+        { name: '6.thinkphp5', href: 'http://www.thinkphp.cn/' },
+        { name: '7.github', href: 'https://github.com/shenxingchao/vue-admin-thinkphp' }
+      ]
     }
   }
 }
