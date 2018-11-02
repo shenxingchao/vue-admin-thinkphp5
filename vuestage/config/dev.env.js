778451f62b5979f083032ddb80353cc809f2678f
shenxingchao <864318466@qq.com>
2018-11-02 11:46:45 +0800

    update
    missing files

--- vuestage/config/dev.env.js
+++ vuestage/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://10.3.9.51/index.php/admin"',
+  BASE_API: '"http://www.vue-admin-thinkphp.com/index.php/admin"',
 })
--- vuestage/config/prod.env.js
+++ vuestage/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://10.3.9.51/index.php/admin"',
+  BASE_API: '"http://www.vue-admin-thinkphp.com/index.php/admin"',
 }
